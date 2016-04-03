01af3d22b7b51f6f3bff53815f581fbc77dba6ce
Sammy Nave <sammy.nave@meyouhealth.com>
2016-04-03 15:22:19 -0400

    compare with transducers

--- src/index.js
+++ src/index.js
@@ -2,28 +2,55 @@ import I from 'immutable';
 import fs from  'fs';
 import crypto from 'crypto';
 import moment from 'moment';
+import { map, filter, comp, into, dedupe, mapcat } from 'transducers-js';
 
 fs.readFile('tests/fixtures/simple.json', begin);
 
 
 function begin(err, data) {
-  console.time('transform');
 
   var path = 'tests/fixtures/simple-transformed.json';
-  var file = I.fromJS(JSON.parse(data));
-  var baseResource = file.get("orders")
+  var file = I.fromJS(JSON.parse(data)).get('orders');
+
+  console.time('transform');
+  var baseResource = file
                       .map(uuid)              //1. addUuid to base obj
                       .map(removeNull)        //2. drop null from base obj
                       .flatMap(convertDates); //3. convert dates in base obj
 
-  var nestedResources = baseResource.map(filterMaps).map(function(obj) {
-    return obj.map(uuid).map(removeNull).map(convertDates);
-  });
-
+  var nestedResources = baseResource.map(filterMaps).map(transform);
   console.timeEnd('transform');
 
-  console.log(JSON.stringify(baseResource));
-  console.log(JSON.stringify(nestedResources));
+
+  console.time('transform2');
+  var baseResource2 = [];
+  var xf = comp(
+    map(uuid),
+    map(removeNull),
+    mapcat(convertDates)
+  );
+  into(baseResource2, xf, file);
+
+  var nestedResources2 = [];
+  var xf2 = comp(
+    map(filterMaps),
+    map(transform)
+  );
+  into(nestedResources2, xf2, baseResource2);
+  console.timeEnd('transform2');
+
+  //console.log(JSON.stringify(baseResource)+'\n');
+  //console.log(JSON.stringify(baseResource2)+'\n');
+
+  //console.log(JSON.stringify(nestedResources)+'\n');
+  //console.log(JSON.stringify(nestedResources2)+'\n');
+
+  console.log(JSON.stringify(baseResource)==JSON.stringify(baseResource2));
+  console.log(JSON.stringify(nestedResources)==JSON.stringify(nestedResources2));
+}
+
+function transform(obj) {
+  return obj.map(uuid).map(removeNull).map(convertDates);
 }
 
 function filterMaps(obj) {
