248e6d63cf96841d8c21220e166514d5942d3264
Sammy Nave <sammy.nave@meyouhealth.com>
2016-04-02 20:09:04 -0400

    uuh

--- src/index.js
+++ src/index.js
@@ -1,55 +1,78 @@
 // import I from 'immutable';
 import fs from  'fs';
 import crypto from 'crypto';
-import convertDates from './date.js';
+import moment from 'moment';
 import {
   seq,
   compose,
   map,
   toArray,
-  filter
+  filter,
+  into,
+  push
 } from 'transducers.js';
 
 fs.readFile('tests/fixtures/simple.json', transformBegin);
 
 function transformBegin(err, data) {
-  var resources = JSON.parse(data);
-  console.log(resources);
-  var tables = seq(
-    resources,
-    compose(
-      map(writeTable)
-    )
-  );
-
+  //TODO: don't hardcode this
+  var type = 'orders';
   var path = 'tests/fixtures/simple-transformed.json';
+  var tables = {};
+  tables[`${type}`] = [];
+
+  data = JSON.parse(data);
+
+  into(
+    tables[`${type}`],
+    compose(
+      map(createUUIDs),
+      map(convertDates)
+    ),
+    data[`${type}`]
+  );
 
   fs.writeFile(path, JSON.stringify(tables), function(err) {
     console.log('error: %s', err);
   });
 }
 
-function writeTable(x) {
-  return [x[0], transformResources(x[1])];
-}
-
-function transformResources(rs) {
-  return seq(
-    rs,
-    compose(
-      map(createUUIDs),
-      map(convertDates)
-    )
-  );
+function extractNested(resources) {
+  return resources;
 }
 
 function isObject(x) {
   return x[1] instanceof Object;
 }
-function createUUIDs(rs) {
-  var shasum = crypto.createHash('sha1');
-  shasum.update(JSON.stringify(rs));
-  rs['uuid'] = shasum.digest('hex');
 
-  return rs;
+function createUUIDs(resource) {
+  resource['uuid'] = crypto.createHash('sha1').update(JSON.stringify(resource)).digest('hex');
+  return resource;
 }
+
+function convertDates(resource) {
+  return seq(
+    resource,
+    compose(
+      map(convertDate)
+    )
+  );
+};
+
+function convertDate(kv) {
+  var key = kv[0];
+  var val = kv[1];
+  if (isDate(key) && val !== null) {
+    kv[1] = redshiftDate(val);
+  }
+  return kv;
+}
+
+function redshiftDate(date) {
+  return moment(date).utc().format('YYYY-MM-DD HH:mm:ss');
+}
+
+function isDate(str) {
+  return str.slice(-3) === '_at';
+}
+
