df7f326e9360a64e36434301d821f8d460f36ffa
Sammy Nave <sammy.nave@meyouhealth.com>
2016-04-03 03:54:34 -0400

    so much better! using immutable js

--- src/index.js
+++ src/index.js
@@ -2,95 +2,55 @@ import I from 'immutable';
 import fs from  'fs';
 import crypto from 'crypto';
 import moment from 'moment';
-import {
-  seq,
-  compose,
-  map,
-  toArray,
-  toObj,
-  filter,
-  into,
-  push
-} from 'transducers.js';
 
-fs.readFile('tests/fixtures/simple.json', transformBegin);
+fs.readFile('tests/fixtures/simple.json', begin);
 
-function transformBegin(err, data) {
-  //TODO: don't hardcode this
+
+function begin(err, data) {
+  console.time('transform');
+
   var path = 'tests/fixtures/simple-transformed.json';
-  var rawData = I.Map(JSON.parse(data));
-  var createBaseTable = compose(
-    map(createUUIDs),
-    map(convertDates),
-    map(stringifyNested)
-  );
+  var file = I.fromJS(JSON.parse(data));
+  var baseResource = file.get("orders")
+                      .map(uuid)              //1. addUuid to base obj
+                      .map(removeNull)        //2. drop null from base obj
+                      .flatMap(convertDates); //3. convert dates in base obj
 
-  var baseTable = seq(rawData, createBaseTable);
-
-  fs.writeFile(path, JSON.stringify(baseTable), function(err) {
-    console.log('error: %s', err);
+  var nestedResources = baseResource.map(filterMaps).map(function(obj) {
+    return obj.map(uuid).map(removeNull).map(convertDates);
   });
 
- //  var nestedTables = seq(
- //    toArray(baseTable)[0][1],
- //    compose(
- //      map(filterNested)
- //    )
- //  );
+  console.timeEnd('transform');
 
- //  console.log(nestedTables);
+  console.log(JSON.stringify(baseResource));
+  console.log(JSON.stringify(nestedResources));
 }
 
-function stringifyNested(obj) {
-  return obj;
+function filterMaps(obj) {
+  return obj.filter(x => I.Map.isMap(x));
 }
 
-function createUUIDs(resource) {
-  var type = resource[0]; //e.g. 'orders'
-  var objects = resource[1]; //e.g. [{id:1 }, ..., {id:n}]
-
-  objects.forEach(function(obj) {
-    obj['uuid'] = crypto.createHash('sha1').update(JSON.stringify(obj)).digest('hex');
-  });
-
-  return resource;
+function uuid(obj) {
+  var uuid = crypto.createHash('sha1').update(JSON.stringify(obj)).digest('hex');
+  return obj.set("uuid", uuid);
 }
 
-function convertDates(resource) {
-  var type = resource[0];
-  var objects = resource[1];
-
-  resource[1] = seq(
-    objects,
-    map(function(obj) {
-      return seq(
-        obj,
-        map(function(kv){
-          if (isDate(kv)) {
-            kv[1] = redshiftDate(kv[1]);
-          }
-
-          return kv;
-        })
-      )
-    })
-  );
-
-  return resource;
-};
-
-function filterNested(x){
-  return seq(x, filter(isObject));
+function removeNull(obj) {
+  return obj.toKeyedSeq().filter(val => val !== null).toMap();
 }
 
-function isObject(x) {
-  return x[1] instanceof Object;
+function convertDates(obj) {
+  return obj.keySeq().filter(isDate).map(k => convertDate(k, obj));
+}
+
+function convertDate(k, obj) {
+  return obj.set(k, redshiftDate(obj.get(k)));
 }
 
 function redshiftDate(date) {
   return moment(date).utc().format('YYYY-MM-DD HH:mm:ss');
 }
 
-function isDate(kv) {
-  return kv[0].slice(-3) === '_at' && kv[1] !== null;
+function isDate(k) {
+  return k.length > 3 && k.slice(-3) === '_at';
 }
